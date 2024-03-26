11220d3a14e49917d4da5856558653f9c68fa51b
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-26 15:54:15 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wordpress" {
-  ami                         = "ami-0208423bef18c17d1"
+  ami                         = "ami-000c0df09737657b6"
   instance_type               = "t3.micro"
-  key_name                    = "stockholm"
+  key_name                    = "master-1"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
